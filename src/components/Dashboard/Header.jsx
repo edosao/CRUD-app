eb1ff503b0a962eba156dc3195413ac48ce47b55
Edosa Ogbevoen <justiceogbevoen@gmail.com>
2025-11-03 18:02:38 +0100

    Refactor button click handler in Header component
    Refactor button click handler to use onClick prop directly.

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -9,10 +9,9 @@ const Header = ({ setIsAdding, setIsAuthenticated }) => {
       <div className="my-8">
         <button
           className="border  bg-[#0366ee] hover:bg-[#0356d6] text-base text-white py-2 px-4 rounded font-bold"
-          onClick={() => {
+          onClick={() => 
             setIsAdding(true);
-            localStorage.clear();
-          }}
+          }
         >
           Add Employee
         </button>
